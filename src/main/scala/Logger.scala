d711f7cee2f2e05822dfe3092a74d08233696328
Christopher Spears <cspears2002@gmail.com>
2024-08-11 15:01:19 -0700

    Make an index to letter map

--- src/main/scala/Logger.scala
+++ src/main/scala/Logger.scala
@@ -1,5 +1,7 @@
 package logger:
 
+    import scala.compiletime.ops.double
+
     trait Logger:
         def log(msg: String): Unit
 
@@ -7,6 +9,12 @@ package logger:
         def log(msg: String) = println(msg)
         
         def encryptMsg(msg: String, key: Int = 3): Unit =
-            val letterWithIdxLst = ('a' to 'z').toList.zipWithIndex.toMap
-            println(letterWithIdxLst)
+            val letterToIdxMap = ('a' to 'z').toList.zipWithIndex.toMap
+            val idxToLetterMap = ('a' to 'z').toList.zipWithIndex.map(x => (x(1), x(0))).toMap
+            for letter <- msg do
+                if letterToIdxMap.contains(letter.toLower) then
+                    val idx = letterToIdxMap(letter.toLower) + key
+                    println(idx)
+                else
+                    println()
 
